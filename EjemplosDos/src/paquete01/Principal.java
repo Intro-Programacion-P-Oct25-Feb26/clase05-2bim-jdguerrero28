c5f27e1e4c9bcd5010a9447a8f70c75c10ccf5b9
jdguerrero28 <jdiegogueal@gmail.com>
2026-01-15 11:28:32 -0500

    Principal-Promedio

--- EjemplosDos/src/paquete01/Principal.java
+++ EjemplosDos/src/paquete01/Principal.java
@@ -43,3 +43,14 @@ public class Principal {
     }
     
 }
+/*
+El codigo dado se encarga primeramente en el paquete02 y la clase DatoPersonal 
+de pedir por entrada el nombre del usuario y su apellido dentro de un solo 
+paquete y clase, luego dentro del paquete03 en la clase DatosUbicacion se pedira
+la ciudad a la que pertenece, luego en el paquete04 y su clase se requiere que 
+el usuario ingrese sus notas las cuales deben ser 4 porque el limite es ese y 
+retornara las notas, dentro del paquete05 se va a calcular el promedio de las 
+notas y se retornara el promedio, para que dentro de la clase Principal.java 
+se presente un reporte con todos los datos ingresados (Nombre, Apellido, Ciudad)
+junto con el promedio calculado mediante las notas ingresadas por el usuario.
+*/
